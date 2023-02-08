cf191eee65ca9f187a3679307f2201b29ef20d58
IsaacCopenhaver <isaaccopenhaver@gmail.com>
2023-02-08 15:11:11 -0500

    added ability to choose a file to process

--- WordCount.java
+++ WordCount.java
@@ -1,8 +1,16 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class WordCount{
-    public static void main(String[] args){
-    
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner scan = new Scanner(System.in);
+        Scanner fileScanner;
+
+        System.out.println("--File Processor-- \nEnter File Name:");
+        String fileName = scan.next();
+        File file = new File(fileName);
+        fileScanner = new Scanner(file);
+        
     }
 }
