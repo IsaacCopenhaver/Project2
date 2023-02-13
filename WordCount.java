7fb09bc1c1f7fca6d8fb10d1fdbad92bd7e82873
IsaacCopenhaver <isaaccopenhaver@gmail.com>
2023-02-13 14:37:05 -0500

    got word counting fixed

--- WordCount.java
+++ WordCount.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 public class WordCount{
     public static void main(String[] args) throws FileNotFoundException{
         Scanner scan = new Scanner(System.in);
-        Scanner fileScanner;
-
-        System.out.println("--File Processor-- \nEnter File Name:");
+        int charsInFile = 0;
+        ArrayList<String> strings = new ArrayList<String>();
+        System.out.println("--File Processor--\nEnter File Name:");
         String fileName = scan.next();
         File file = new File(fileName);
-        fileScanner = new Scanner(file);
-        
+        Scanner fileReader = new Scanner(file);
+        do{
+            strings.add(fileReader.next());
+        }while(fileReader.hasNext());
+        System.out.println("Words in File: " + strings.size());
+        scan.close();
     }
 }
